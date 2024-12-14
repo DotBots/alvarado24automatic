6e36a8c45742bbcc77797a2c4612a4cb2c4e6cef
Said Alvarado <said_alvarado@hotmail.com>
2024-12-14 14:11:54 +0100

    fix slow frame rate causing repeated LH values

--- dataset/LH_parse_logfile.py
+++ dataset/LH_parse_logfile.py
@@ -11,15 +11,13 @@ from matplotlib.gridspec import GridSpec
 ###                                Options                                ###
 #############################################################################
 
-scene_number = 3
+scene_number = 2
 
 #############################################################################
 ###                                Code                                   ###
 #############################################################################
 
-# filename_calib = f"Raw Data\LH data\calib\pydotbot.log"
-# filename_data  = f"Raw Data/LH data/exp_data/pydotbot.log"
-filename = f"test_2/pydotbot.log"
+filename = f"scene_{scene_number}/raw_data/pydotbot.log"
 
 ## Read the struct log with the information
 # Define a regular expression pattern to extract timestamp and source from log lines
@@ -46,7 +44,11 @@ with open(filename, "r") as log_file:
 # Create a pandas DataFrame from the extracted data
 
 org_data = []
-last_state = {"timestamp":-1, "db_time":-1, "lha_poly_0": -1, "lha_count_0": -1, "lha_poly_1": -1, "lha_count_1": -1, "lhb_poly_0": -1, "lhb_count_0": -1, "lhb_poly_1": -1, "lhb_count_1": -1,}
+last_state = {"timestamp":-1, "db_time":-1, "lha_poly_0": -1, "lha_count_0": -1, "lha_time_0": -1,  
+                                            "lha_poly_1": -1, "lha_count_1": -1, "lha_time_1": -1, 
+                                            "lhb_poly_0": -1, "lhb_count_0": -1, "lhb_time_0": -1, 
+                                            "lhb_poly_1": -1, "lhb_count_1": -1, "lhb_time_1": -1,}
+
 base_timestamp = {"timestamp":-1, "db_time":-1}
 
 # organize the data into sweep 1 and sweep 2
@@ -55,7 +57,9 @@ for idx, sweep in enumerate(data):
     # If there was a big time jump, reset the first line assignment
     if last_state["timestamp"] != -1:
         if (sweep["db_time"] - last_state["db_time"]) > 1e6 or (sweep["db_time"] - last_state["db_time"]) < -1e6: # gap larger than 1 sec or a reset occured
-            last_state = {"timestamp":-1, "db_time":-1, "lha_poly_0": -1, "lha_count_0": -1, "lha_poly_1": -1, "lha_count_1": -1, "lhb_poly_0": -1, "lhb_count_0": -1, "lhb_poly_1": -1, "lhb_count_1": -1,}
+            for k in last_state.keys():
+                last_state[k] = -1
+                # last_state = {"timestamp":-1, "db_time":-1, "lha_poly_0": -1, "lha_count_0": -1, "lha_poly_1": -1, "lha_count_1": -1, "lhb_poly_0": -1, "lhb_count_0": -1, "lhb_poly_1": -1, "lhb_count_1": -1,}
 
 
     # lfsr counts larger than 100k, are most likely an outlier
@@ -82,6 +86,18 @@ for idx, sweep in enumerate(data):
     last_state[f"lh{lh}_poly_{sweep['sweep']}"] = sweep['poly']
     last_state[f"lh{lh}_count_{sweep['sweep']}"] = sweep['lfsr_index']
 
+
+    # Make sure available data is not too old ( newer than 25ms )
+    last_state[f"lh{lh}_time_{sweep['sweep']}"] = sweep["db_time"]
+    for lh_i, sweep_i in [("a","0"),("a","1"),("b","0"),("b","1")]:
+        # check if there is any data too old
+        if (sweep["db_time"] - last_state[f"lh{lh_i}_time_{sweep_i}"]) > 25000  and last_state[f"lh{lh_i}_time_{sweep_i}"] != 1:
+            # erase that particular data point from the buffer
+            last_state[f"lh{lh_i}_poly_{sweep_i}"] = -1
+            last_state[f"lh{lh_i}_count_{sweep_i}"] = -1
+            last_state[f"lh{lh_i}_time_{sweep_i}"] = -1
+
+
     # you found at least 1 datapoint of each LH/sweep.
     # Stop this first line assigment
     if (last_state[f"lha_poly_0"] != -1) and (last_state[f"lha_poly_1"] != -1) and (last_state[f"lhb_poly_0"] != -1) and (last_state[f"lhb_poly_1"] != -1):   
@@ -93,6 +109,8 @@ df = pd.DataFrame(org_data)
 
 # sort by db time
 df.sort_values(by='timestamp', ascending=True, inplace=True)
+
+df = df[["timestamp", "db_time", "lha_poly_0", "lha_count_0", "lha_poly_1", "lha_count_1", "lhb_poly_0", "lhb_count_0", "lhb_poly_1", "lhb_count_1"]]
 
 
 # ## sort the lhc_counts so that the lowest value is always on lhX_count_0
@@ -212,11 +230,11 @@ def clear_outliers(df, speed_threshold=5e3, jump_threshold=7e3):
 
     return df
 
-df.to_csv("./outlier_test_2.csv",index=True)
+# df.to_csv("./outlier_scene_2.csv",index=True)
 # Get the cleaned values back on the variables needed for the next part of the code.
 df = clear_outliers(df, 40e3)
 
 # Change the format of the timestamp column
 df['timestamp'] = df['timestamp'].apply(lambda x: x.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
 
-df.to_csv("test_2/lh_test_2_data.csv",index=True)
+df.to_csv(f"scene_{scene_number}/lh_data.csv",index=True)
