c47adf566c937b119aaf5de733f6c2269dbd2022
Said Alvarado-Marin <said_alvarado@hotmail.com>
2024-12-18 20:38:24 +0100

    clean mocap dataset preparation script

--- dataset/MOCAP_tsv_to_csv_3D.py
+++ dataset/MOCAP_tsv_to_csv_3D.py
@@ -4,6 +4,12 @@ from zoneinfo import ZoneInfo
 from skspatial.objects import Plane
 import numpy as np
 
+#############################################################################
+###                                Options                                ###
+#############################################################################
+
+scene_number = 1
+
 ########################################################################
 ###                            Functions                             ###
 ########################################################################
@@ -23,13 +29,10 @@ def rotation_matrix_from_vectors(vec1, vec2):
 ###                              Main                                 ###
 #########################################################################
 
-
-#########################################################################
-###                             Data File                             ###
-#########################################################################
+# get calibration data
 
 # Sample TSV data (you would replace this with your actual TSV file content)
-tsv_data_file = "scene_1/scene_1_6D.tsv"
+tsv_data_file = f"scene_{scene_number}/raw_data/scene_{scene_number}_6D.tsv"
 # Read the TSV data into a DataFrame
 df_data = pd.read_csv(tsv_data_file, sep='\t', skiprows=13)
 data_start_time = pd.read_csv(tsv_data_file, sep='\t', skiprows = lambda x: x not in [7]).columns[1]
@@ -51,15 +54,11 @@ df_data.reset_index(drop=True, inplace=True)
 # Convert timetamp column to a datetime object
 df_data['timestamp'] = df_data['timestamp'].apply(lambda x: data_start_time + timedelta(seconds=x))
 
-# Clear outliers
-
-
 # Convert timetamp column from a datetime object to a properly formated string
 df_data['timestamp'] = df_data['timestamp'].dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-
 # Save the selected columns to a CSV file
-csv_data_file_path = 'scene_1/scene_1_data.csv'  # Specify your desired file path here
+csv_data_file_path = f"scene_{scene_number}/mocap_data.csv"  # Specify your desired file path here
 df_data.to_csv(csv_data_file_path, index=True)
 
 print(f"File saved to {csv_data_file_path}")
