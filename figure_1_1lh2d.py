3c1ff758c0486ff047342b5c00fdf7ad246ef199
Said Alvarado-Marin <said_alvarado@hotmail.com>
2024-12-28 19:01:50 +0100

    clean main scripts to get results and figures for the paper

--- figure_1_1lh2d.py
+++ figure_1_1lh2d.py
@@ -25,73 +25,71 @@ from functions.plotting import plot_trajectory_and_error, plot_error_histogram, 
 ###                               Options                                        ###
 ####################################################################################
 # Define which of the 6 experimetns you want to plot
-experiment_number = 1
-
-####################################################################################
-###                            Read Dataset                                      ###
-####################################################################################
-
-# file with the data to analyze
-LH_data_file = f'./dataset/scene_{experiment_number}/lh_data.csv'
-mocap_data_file = f'./dataset/scene_{experiment_number}/mocap_data.csv'
-calib_file = './dataset/calibration.json'
-
-# Import data
-df, calib_data, (start_idx, end_idx) = import_data(LH_data_file, mocap_data_file, calib_file)
-
-start_time = df.loc[start_idx]['time_s'] - df.iloc[0]['time_s']
-end_time   = df.loc[end_idx]['time_s']   - df.iloc[0]['time_s']
-
-####################################################################################
-###                            Process Data                                      ###
-####################################################################################
 errors=[]
-for LH in ['LHA', 'LHB']:
-    # Project sweep angles on to the z=1 image plane
-    pts_lighthouse_A = LH2_count_to_pixels(df['LHA_count_1'].values, df['LHA_count_2'].values, 0)
-    pts_lighthouse_B = LH2_count_to_pixels(df['LHB_count_1'].values, df['LHB_count_2'].values, 1)
-
-    # Plot the LH view for debbuging
-    # plot_projected_LH_views(pts_lighthouse_A, pts_lighthouse_B)
-
-    # Add the LH2 projected matrix into the dataframe that holds the info about what point is where in real life.
-    df['LHA_proj_x'] = pts_lighthouse_A[:,0]
-    df['LHA_proj_y'] = pts_lighthouse_A[:,1]
-    df['LHB_proj_x'] = pts_lighthouse_B[:,0]
-    df['LHB_proj_y'] = pts_lighthouse_B[:,1]
-
+for experiment_number in [1,2]:
     ####################################################################################
-    ###                             1LH 2D algorithm                                 ###
+    ###                            Read Dataset                                      ###
     ####################################################################################
 
-    # Convert the 4k camera pixel data and the LH2 pixel data to the world coordinate frame of reference.
-    pts_dst = np.array([[0,400],[400,400],[400,0],[0,0]])
-    df = camera_to_world_homography(df, calib_data, pts_dst)
+    # file with the data to analyze
+    LH_data_file = f'./dataset/scene_{experiment_number}/lh_data.csv'
+    mocap_data_file = f'./dataset/scene_{experiment_number}/mocap_data.csv'
+    calib_file = './dataset/calibration.json'
 
-    #TODO: Finish error computing code
-    # And make the code that matches both frames of reference, for comparison
-    df,_,_ = correct_similarity_distrotion(df, calib_data)
+    # Import data
+    df, calib_data, (start_idx, end_idx) = import_data(LH_data_file, mocap_data_file, calib_file)
 
-    # Calculate the L2 distance error
-    error = np.linalg.norm(
-        df[[LH+'_Rt_x', LH+'_Rt_y']].values - 
-        df[['real_x_mm', 'real_y_mm']].values, 
-        axis=1)
-    
-    errors.append(error[start_idx:end_idx])
+    start_time = df.loc[start_idx]['time_s'] - df.iloc[0]['time_s']
+    end_time   = df.loc[end_idx]['time_s']   - df.iloc[0]['time_s']
+
     ####################################################################################
-    ###                                 Plot Results                                 ###
+    ###                            Process Data                                      ###
     ####################################################################################
+    for LH in ['LHA', 'LHB']:
+        # Project sweep angles on to the z=1 image plane
+        pts_lighthouse_A = LH2_count_to_pixels(df['LHA_count_1'].values, df['LHA_count_2'].values, 0)
+        pts_lighthouse_B = LH2_count_to_pixels(df['LHB_count_1'].values, df['LHB_count_2'].values, 1)
 
-    lh2_data = {'x':    df[LH+'_Rt_x'].values,
-                'y':    df[LH+'_Rt_y'].values,
-                'time': df['time_s'].values}
+        # Plot the LH view for debbuging
+        # plot_projected_LH_views(pts_lighthouse_A, pts_lighthouse_B)
 
-    camera_data = { 'x':    df['real_x_mm'].values,
-                    'y':    df['real_y_mm'].values,
+        # Add the LH2 projected matrix into the dataframe that holds the info about what point is where in real life.
+        df['LHA_proj_x'] = pts_lighthouse_A[:,0]
+        df['LHA_proj_y'] = pts_lighthouse_A[:,1]
+        df['LHB_proj_x'] = pts_lighthouse_B[:,0]
+        df['LHB_proj_y'] = pts_lighthouse_B[:,1]
+
+        ####################################################################################
+        ###                             1LH 2D algorithm                                 ###
+        ####################################################################################
+
+        # Convert the LH2 pixel data to the world coordinate frame of reference.
+        pts_dst = np.array([[0,400],[400,400],[400,0],[0,0]])  # calibration 40x40 cm square
+        df = camera_to_world_homography(df, calib_data, pts_dst)
+
+        # And make the code that matches both frames of reference, for comparison
+        df,_,_ = correct_similarity_distrotion(df, calib_data)
+
+        # Calculate the L2 distance error
+        error = np.linalg.norm(
+            df[[LH+'_Rt_x', LH+'_Rt_y']].values - 
+            df[['real_x_mm', 'real_y_mm']].values, 
+            axis=1)
+        
+        errors.append(error[start_idx:end_idx])
+        ####################################################################################
+        ###                                 Plot Results                                 ###
+        ####################################################################################
+
+        lh2_data = {'x':    df[LH+'_Rt_x'].values,
+                    'y':    df[LH+'_Rt_y'].values,
                     'time': df['time_s'].values}
 
-    plot_trajectory_and_error(lh2_data, camera_data, error, start_time, end_time)
+        camera_data = { 'x':    df['real_x_mm'].values,
+                        'y':    df['real_y_mm'].values,
+                        'time': df['time_s'].values}
+
+        plot_trajectory_and_error(lh2_data, camera_data, error, start_time, end_time)
 
 errors = np.hstack(errors)
 # Print the RMSE, MAE and STD
